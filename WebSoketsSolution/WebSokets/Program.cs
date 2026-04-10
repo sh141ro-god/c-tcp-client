60880f8b8b499e0e18246abfb85ff0bc7c5b98a2
sh141ro-god <soomrak234@gmail.com>
2026-04-10 13:43:37 +0300

    please

--- WebSoketsSolution/WebSokets/Program.cs
+++ WebSoketsSolution/WebSokets/Program.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Net;
+using System.Collections.Generic;
 using WebSockets;
 using WebSokets;
 
@@ -8,6 +9,7 @@ Console.WriteLine("1 - Listener (wait for connections)");
 Console.WriteLine("2 - Searcher (find and connect)");
 Console.WriteLine("3 - Echo server (multi-client)");
 Console.WriteLine("4 - HTTP server (respond to browser)");
+Console.WriteLine("5 - Searcher -> Echo server");
 string choice = Console.ReadLine();
 
 if (choice == "1")
@@ -19,7 +21,6 @@ else if (choice == "2")
     Console.Write("Enter last octet of your IP (172.20.117.?): ");
     string octet = Console.ReadLine();
     Searcher.localIP = $"172.20.117.{octet}";
-
     Console.WriteLine("Searching...");
     Searcher.Search();
     Searcher.Print();
@@ -32,11 +33,9 @@ else if (choice == "2")
 
     Console.Write("Enter number to connect to: ");
     int index = int.Parse(Console.ReadLine()) - 1;
-
-    var list = new System.Collections.Generic.List<System.Collections.Generic.KeyValuePair<string, int>>(Searcher.Found);
+    var list = new List<KeyValuePair<string, int>>(Searcher.Found);
     string ip = list[index].Key;
     int port = list[index].Value;
-
     Searcher.Connect(ip, port);
     Console.WriteLine($"Connected to {ip}:{port}");
 
@@ -46,6 +45,11 @@ else if (choice == "2")
         string msg = Console.ReadLine();
         if (msg == "exit") break;
         Searcher.Write(msg);
+
+        // читаем ответ если это EchoServer
+        string echo = Searcher.Read();
+        if (echo != null)
+            Console.WriteLine($"Echo: {echo}");
     }
 }
 else if (choice == "3")
@@ -58,17 +62,38 @@ else if (choice == "4")
 }
 else if (choice == "5")
 {
-   
+    Console.Write("Enter last octet of your IP (172.20.117.?): ");
+    string octet = Console.ReadLine();
+    Searcher.localIP = $"172.20.117.{octet}";
+    Console.WriteLine("Searching...");
     Searcher.Search();
     Searcher.Print();
 
+    if (Searcher.Found.Count == 0)
+    {
+        Console.WriteLine("No hosts found.");
+        return;
+    }
 
-    var first = Searcher.Found.First();
-    string ip = first.Key; 
-    Searcher.Connect(ip, 55000); 
+    Console.Write("Enter number to connect to: ");
+    int index = int.Parse(Console.ReadLine()) - 1;
+    var list = new List<KeyValuePair<string, int>>(Searcher.Found);
+    string ip = list[index].Key;
+    int port = list[index].Value;
 
+    Searcher.Connect(ip, port);
+    Console.WriteLine($"Connected to {ip}:{port}");
 
-    Searcher.Write("Hello!");
+    while (true)
+    {
+        Console.Write("Message: ");
+        string msg = Console.ReadLine();
+        if (msg == "exit") break;
+        Searcher.Write(msg);
+        string echo = Searcher.Read();
+        if (echo != null)
+            Console.WriteLine($"Echo: {echo}");
+    }
 }
 else
 {
